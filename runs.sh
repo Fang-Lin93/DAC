818847c7bf2fc486c708b36132abe75aefaa17bf
linjiajiefang <lfangad@connect.ust.hk>
2024-05-30 15:07:02 +0800

    fix bugs, remove dependency on config files for easy implementation

--- runs.sh
+++ runs.sh
@@ -22,10 +22,3 @@ python main.py --env antmaze-medium-diverse-v0 --agent dac --maxQ --eta 0.1 --et
 
 python main.py --env antmaze-large-play-v0 --agent dac --maxQ --eta 0.1 --eta_lr 0  --rho 4 --q_tar lcb  --tag FinalSqrt   --critic_lr 0.001 --num_seed 5 --gpu 7 --num_qs 10
 python main.py --env antmaze-large-diverse-v0 --agent dac --maxQ --eta 0.1 --eta_lr 0 --rho 3.5 --q_tar lcb   --tag FinalSqrt   --critic_lr 0.001 --num_seed 5 --gpu 7 --num_qs 10
-
-
-# another try fixed eta
-#XLA_PYTHON_CLIENT_PREALLOCATE=false python main.py --env antmaze-medium-play-v0 --agent dac --maxQ --eta 0.1 --eta_lr 0 --bc_threshold 1 --rho 1 --q_tar lcb  --tag loco
-#XLA_PYTHON_CLIENT_PREALLOCATE=false python main.py --env antmaze-medium-diverse-v0 --agent dac --maxQ --eta 0.1 --eta_lr 0 --bc_threshold 1 --rho 1 --q_tar lcb   --tag loco
-#XLA_PYTHON_CLIENT_PREALLOCATE=false python main.py --env antmaze-large-play-v0 --agent dac --maxQ --eta 0.1 --eta_lr 0 --bc_threshold 1 --rho 1 --q_tar lcb --tag loco
-#XLA_PYTHON_CLIENT_PREALLOCATE=false python main.py --env antmaze-large-diverse-v0 --agent dac --maxQ --eta 0.1 --eta_lr 0 --bc_threshold 1 --rho 1 --q_tar lcb --tag loco
